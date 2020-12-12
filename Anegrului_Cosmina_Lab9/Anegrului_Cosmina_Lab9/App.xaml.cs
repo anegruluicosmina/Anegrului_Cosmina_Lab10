82f90bbe1e1ab23d7bc8cc517ca241eb2c77c53e
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2020-12-12 18:20:17 +0200

    Add project files.

---
+++ Anegrului_Cosmina_Lab9/Anegrului_Cosmina_Lab9/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Anegrului_Cosmina_Lab9.Data;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Anegrului_Cosmina_Lab9
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
